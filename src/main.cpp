689fa8b8f246e13d3d4ad93d2025c5d0c5375849
97522240+tascvh@users.noreply.github.com <97522240+tascvh@users.noreply.github.com>
2025-02-11 16:56:35 +0000

    Make simple_parser use non optional values and give type info

--- src/main.cpp
+++ src/main.cpp
@@ -9,58 +9,61 @@
 #include "event_handlers.hpp"
 #include "simple_parser.hpp"
 #include <iostream>
+#include <optional>
 
 int main(int argc, char **argv) {
 
   struct RunningMode {
-    enum class Type { Print, Strict, Flex };
-    static Type Get(const std::string &mode) {
+    enum class Type { Print, Strict, Flex, Invalid };
+    static Type FromString(const std::string &mode) {
       if (mode == "p")
         return Type::Print;
       if (mode == "s")
         return Type::Strict;
       if (mode == "f")
         return Type::Flex;
-      auto s = std::format("Invalid running mode {}", mode);
-      throw std::invalid_argument(s);
+      return Type::Invalid;
     }
   };
 
   try {
     // cmd arguments
     std::optional<std::string> device;
-    std::optional<std::string> mode("f");
-    std::optional<int> left(10);
-    std::optional<int> right(10);
-    std::optional<int> top(0);
-    std::optional<int> bottom(15);
+    std::string mode("f");
+    int left(10);
+    int right(10);
+    int top(0);
+    int bottom(15);
 
-    SimpleParser sp(argc, argv);
+    {
+      SimpleParser sp(argc, argv);
+      sp.read(sp.m_showHelp, "-h", "");
+      if (sp.m_showHelp) {
+        std::cout << "-h : Show this help message" << std::endl;
+      }
 
-    sp.read(sp.m_showHelp, "-h", "");
+      sp.read(device, "-d", "trackpad device filename (mandatory)");
+      sp.read(mode, "-m", "Running mode options : p/s/f ");
+      sp.read(left, "-l", "left percentage");
+      sp.read(right, "-r", "right percentage");
+      sp.read(top, "-t", "top percentage");
+      sp.read(bottom, "-b", "bottom percentage");
 
-    if (*sp.m_showHelp) {
-      std::cout << "-h Show this help message" << std::endl;
-    }
-
-    sp.read(device, "-d", "trackpad device filename (mandatory)");
-    sp.read(mode, "-m", "Running mode options : p/s/f");
-    sp.read(left, "-l", "left percentage");
-    sp.read(right, "-r", "right percentage");
-    sp.read(top, "-t", "top percentage");
-    sp.read(bottom, "-b", "bottom percentage");
-
-    if (*sp.m_showHelp) {
-      return EXIT_SUCCESS;
+      if (sp.m_showHelp) {
+        std::cout << std::endl << "Example usage :" << std::endl;
+        std::cout << "\tsudo ./titdb -d /dev/input/event0 -m f" << std::endl;
+        return EXIT_SUCCESS;
+      }
     }
 
     if (!device) {
-      throw std::invalid_argument("device argument is mandatory -h for help");
+      auto s = "device argument is mandatory. Use -h for help";
+      throw std::invalid_argument(s);
     }
 
     auto evdev = Evdev(*device);
 
-    switch (RunningMode::Get(*mode)) {
+    switch (RunningMode::FromString(mode)) {
     case RunningMode::Type::Print: {
       evdev.print();
       return evdev.runEventLoop(PrintEvents());
@@ -68,12 +71,16 @@ int main(int argc, char **argv) {
     case RunningMode::Type::Strict: {
       return evdev.runEventLoop(
           ForwardTo(evdev.Spawn<UInput>(),
-                    evdev.Spawn<CropRect>(*left, *right, *top, *bottom)));
+                    evdev.Spawn<CropRect>(left, right, top, bottom)));
     }
     case RunningMode::Type::Flex: {
       return evdev.runEventLoop(
           ForwardTo(evdev.Spawn<UInput>(),
-                    evdev.Spawn<CropRectFlex>(*left, *right, *top, *bottom)));
+                    evdev.Spawn<CropRectFlex>(left, right, top, bottom)));
+    }
+    case RunningMode::Type::Invalid: {
+      auto s = std::format("Invalid running mode {}. Use -h for help", mode);
+      throw std::invalid_argument(s);
     }
     }
 
--- src/simple_parser.hpp
+++ src/simple_parser.hpp
@@ -1,25 +1,65 @@
-#include <optional>
+/*
+ *
+ * This file is part of trackpad-is-too-damn-big utility
+ * Copyright (c) https://github.com/tascvh/trackpad-is-too-damn-big
+ *
+ */
 #include <sstream>
 
 class SimpleParser {
+protected:
   int m_argc;
   const char *const *const m_argv;
 
 public:
-  std::optional<bool> m_showHelp;
+  bool m_showHelp;
   SimpleParser() = delete;
   SimpleParser(int argc, const char *const *const argv)
       : m_argc(argc), m_argv(argv), m_showHelp(false) {};
+
+  template <typename T> std::string getTypeName(const std::optional<T> &) {
+    if (std::is_same<T, int>::value)
+      return "int";
+    if (std::is_same<T, unsigned int>::value)
+      return "unsigned int";
+    if (std::is_same<T, long>::value)
+      return "long";
+    if (std::is_same<T, double>::value)
+      return "double";
+    if (std::is_same<T, float>::value)
+      return "float";
+    if (std::is_same<T, std::string>::value)
+      return "string";
+    if (std::is_same<T, bool>::value)
+      return "bool";
+    return "";
+  }
+
+  template <typename ArgType>
+  void printHelp(std::optional<ArgType> arg, const std::string &prefix,
+                 const std::string &description) {
+
+    auto tn = getTypeName(arg);
+    std::cout << std::format("{} : ({}) {}.", prefix, tn, description);
+    if (arg)
+      std::cout << "Default value : " << *arg;
+    std::cout << std::endl;
+  }
+
+  template <typename ArgType>
+  void read(ArgType &val, const std::string &prefix,
+            const std::string &description) {
+    std::optional<ArgType> opt(std::move(val));
+    read(opt, prefix, description);
+    val = std::move(*opt);
+  }
+
   template <typename ArgType>
   void read(std::optional<ArgType> &val, const std::string &prefix,
             const std::string &description) {
 
-    if (*m_showHelp) {
-      std::cout << prefix << " " << description;
-      if (val) {
-        std::cout << " default value : " << *val;
-      }
-      std::cout << std::endl;
+    if (m_showHelp) {
+      printHelp(val, prefix, description);
       return;
     }
 
@@ -27,22 +67,25 @@ public:
       std::string arg(m_argv[i]);
       if (arg == prefix) {
         if constexpr (std::is_same<ArgType, bool>::value) {
-          val = true;
+          val.emplace(true);
           return;
         }
         i++;
         if (i == m_argc) {
-          throw std::invalid_argument("cannot parse argument " + prefix);
+          auto s = std::format("No value supplied for argument {}", prefix);
+          throw std::invalid_argument(s);
         }
 
         std::string value(m_argv[i]);
         std::istringstream iss(value);
         ArgType result;
         if (iss >> result) {
-          val = result;
+          val.emplace(std::move(result));
           return;
         } else {
-          throw std::invalid_argument("cannot parse argument " + prefix);
+          auto s =
+              std::format("cannot read ({}) as {}", value, getTypeName(val));
+          throw std::invalid_argument(s);
         }
       }
     }
